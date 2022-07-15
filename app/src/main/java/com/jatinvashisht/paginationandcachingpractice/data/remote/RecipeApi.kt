72db734376ab559703f11911e02f48e3cd1b5722
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-07-16 00:21:54 +0530

    setup hilt and added RecipeApi

---
+++ app/src/main/java/com/jatinvashisht/paginationandcachingpractice/data/remote/RecipeApi.kt
@@ -0,0 +1,10 @@
+package com.jatinvashisht.paginationandcachingpractice.data.remote
+
+import com.jatinvashisht.paginationandcachingpractice.data.remote.dto.RecipeDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RecipeApi {
+    @GET("/{recipe}")
+    suspend fun getRecipeList(@Path("recipe") recipe: String): RecipeDto
+}
--- app/src/main/java/com/jatinvashisht/paginationandcachingpractice/ui/MainActivity.kt
+++ app/src/main/java/com/jatinvashisht/paginationandcachingpractice/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jatinvashisht.paginationandcachingpractice
+package com.jatinvashisht.paginationandcachingpractice.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
