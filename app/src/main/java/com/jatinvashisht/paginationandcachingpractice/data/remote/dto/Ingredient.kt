9812e22a1428956fceb9f9d8598c8111fbef9e6c
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-07-16 00:10:26 +0530

    done package structuring and added remote recipe DTOs

---
+++ app/src/main/java/com/jatinvashisht/paginationandcachingpractice/data/remote/dto/Ingredient.kt
@@ -0,0 +1,6 @@
+package com.jatinvashisht.paginationandcachingpractice.data.remote.dto
+
+data class Ingredient(
+    val description: String = "",
+    val quantity: String = ""
+)
---
+++ app/src/main/java/com/jatinvashisht/paginationandcachingpractice/data/remote/dto/RecipeDto.kt
@@ -0,0 +1,3 @@
+package com.jatinvashisht.paginationandcachingpractice.data.remote.dto
+
+class RecipeDto : ArrayList<RecipeDtoItem>()
