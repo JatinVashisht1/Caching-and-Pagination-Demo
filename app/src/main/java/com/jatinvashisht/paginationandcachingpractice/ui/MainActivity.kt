58ac5ac1c250dc126eb69797415b38cd4bcb80fe
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-07-17 13:52:10 +0530

    caching working fine

--- app/src/main/java/com/jatinvashisht/paginationandcachingpractice/ui/MainActivity.kt
+++ app/src/main/java/com/jatinvashisht/paginationandcachingpractice/ui/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.jatinvashisht.paginationandcachingpractice.ui.theme.PaginationAndCachingPracticeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
